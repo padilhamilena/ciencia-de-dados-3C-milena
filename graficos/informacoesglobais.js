29e52ccbfecfbcc50c711d59531adb0db4faece3
milena padilha <108830792+padilhamilena@users.noreply.github.com>
2024-11-21 17:00:22 -0300

    milena dia 21 do11

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -1,1 +1,9 @@
-const url= https://raw.githubusercontent.com/padilhamilena/ciencia-de-dados-3C-milena/refs/heads/main/dados-globais.json
+const url= https='//raw.githubusercontent.com/padilhamilena/ciencia-de-dados-3C-milena/refs/heads/main/dados-globais.json';
+
+async function vizualizarinformacoesglobais(){
+    const res = await fetch(url);
+
+    const dados = await res.json ();
+}
+
+vizualizarinformacoesglobais();
